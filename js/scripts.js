6da9f1017c4a2c6e0149a903dcbea4e8c489f64f
Anthony Gladden <a.gladden360@outlook.com>
2022-02-26 16:47:03 -0800

    added object and constructors

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-function
+'use strict';
+// business logic
+
+function Pizza (pizza_Size,pizza_Topping,pizza_Price) {
+  this.size = pizza_Size;
+  this.topping = pizza_Topping;
+  this.price = pizza_Price;
+  this.updatePrice = function() {
+    return ++this.price
+  };
+}
+
+let customerPizza = new Pizza
+
+console.log(pizza.topping);
+
+Pizza.prototype.topping = function() {
+  this.topping = 'cheese' || 'pepperoni' || 'pineapple';
+}
+Pizza.prototype.size = function() {
+  this.size = 'Small' || 'Medium' || 'Large';
+}
+Pizza.prototype.price = function() {
+  this.price = 10 || 12 || 14;
+}
